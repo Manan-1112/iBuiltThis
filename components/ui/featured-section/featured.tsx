e10dd287efddeab472527db17f708a902b733c65
Manan-1112 <mananbansal449@gmail.com>
2026-08-10 01:59:07 +0530

    fix:added smooth animations and styling

--- components/ui/featured-section/featured.tsx
+++ components/ui/featured-section/featured.tsx
@@ -1,13 +1,17 @@
 import Card from "./featured-card"
+import Image from "next/image"
 export default function Featured(){
     return(
         <div className="pt-20 pb-20 flex flex-col ">
-            <h1 className=" text-3xl sm:text-3xl lg:text-4xl font-bold tracking-tight mb-2 max-w-5xl">Featured Today</h1>
+           
+            <h1 className=" text-3xl sm:text-3xl lg:text-4xl font-bold tracking-tight mb-2 max-w-5xl flex items-center">
+                 <Image alt="star" src="./star.svg" width={40} height={40}></Image>
+                Featured Today</h1>
             <p className="text-xl">Top picks from our community this week</p>
-            <div className="grid grid-cols-3 gap-8">
-              <Card/>
-              <Card/>
-              <Card/>
+            <div className="m-4 grid grid-cols-3 gap-8">
+              <Card title="Saas" desc="hfwfnwj" tags={["hi","hello"]}/>
+              <Card title="Saas" desc="hfwfnwj"/>
+              <Card title="Saas" desc="hfwfnwj"/>
             </div>
             
         </div>
