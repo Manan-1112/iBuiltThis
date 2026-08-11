7e0d1d36f2510594131e674ffe934f505c1c7f9b
Manan-1112 <mananbansal449@gmail.com>
2026-08-12 01:49:34 +0530

    added recently launched products and improved icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import HeroSection from "@/components/ui/landing-page/hero-section";
 import NavBar from "@/components/ui/navbar/navbar";
 import Featured from "@/components/ui/featured-section/featured";
+import RecentlyLaunched from "@/components/ui/recently-launched/recently-launched";
 export default function Home() {
   return (
     <main>
       <NavBar/>
       <HeroSection />
-      
+      <RecentlyLaunched/>
     </main>
   );
 }
