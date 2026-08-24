46b114065c5619cfd33078d6afd024a008b7e553
Manan-1112 <mananbansal449@gmail.com>
2026-08-25 00:14:13 +0530

    setup clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
 const outfit = Outfit({
   variable: "--font-outfit",
   subsets: ["latin"],
@@ -19,11 +19,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${outfit.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html
+        lang="en"
+        className={`${outfit.variable} h-full antialiased`}
+      >
+        <body className="min-h-full flex flex-col">{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/ui/navbar/navbar.tsx
+++ components/ui/navbar/navbar.tsx
@@ -1,65 +1,52 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import Image from "next/image";
 import { CodeIcon, Compass, CompassIcon, HomeIcon, SparkleIcon, UserIcon } from "lucide-react";
-export default function NavBar(){
-    
-    const isSignedIn=true;
-    return(
+import { SignInButton, SignUpButton,UserButton,Show } from "@clerk/nextjs";
+export default function NavBar() {
+
+    return (
         <div className="flex sticky top-0 z-50 backdrop-blur-sm items-center justify-center w-screen h-16">
-            
+
             <h1 className="absolute left-1/5 flex justify-center items-center gap-0 text-2xl">
-            <CodeIcon height={40} width={40} stroke="#c94e8c" className="mr-2"></CodeIcon>
-            <span>i</span>
-            <span className="text-[#c94e8c] font-semibold gap-0">Built</span>
-            <span>This</span>
+                <CodeIcon height={40} width={40} stroke="#c94e8c" className="mr-2"></CodeIcon>
+                <span>i</span>
+                <span className="text-[#c94e8c] font-semibold gap-0">Built</span>
+                <span>This</span>
             </h1>
 
             <Link href="/">
-                
                 <Button variant="ghost">
                     <HomeIcon height={30} width={30}></HomeIcon>
-                    
-                    Home</Button>
+                    Home
+                </Button>
             </Link>
 
             <Link href="/explore">
-                
                 <Button variant="ghost">
                     <CompassIcon height={30} width={30}></CompassIcon>
-                    
                     Explore
-                    
                 </Button>
             </Link>
-            
+
 
             <div className="absolute right-1/5 flex justify-center items-center ">
-             {isSignedIn?(
-                <>
+
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+               <Button variant="ghost" className="bg-[#c94e8c] text-white">SignUp</Button>
+                        
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
                 <Link href="/submit">
-                <Button variant="ghost" className="bg-[#c94e8c] text-white">
-                    <SparkleIcon size={30} stroke="white"></SparkleIcon>
-                    Submit Project</Button>
-                </Link>
+                            <Button variant="ghost" className="bg-[#c94e8c] text-white">
+                                <SparkleIcon size={30} stroke="white"></SparkleIcon>
+                                Submit Project</Button>
+                        </Link>
 
-                <Link href="/user">
-                <Button variant="ghost">
-                    <UserIcon height={30} width={30}></UserIcon>
-                    </Button>
-                </Link>
-
-            </>
-             ):(<>
-             <Link href="/login">
-                <Button variant="ghost" className="mr-1">SignIn</Button>
-            </Link>
-
-            <Link href="/register">
-                <Button variant="ghost" className="bg-[#c94e8c] text-white">SignUp</Button>
-            </Link>
-
-             </>)}
+              <UserButton />
+            </Show>
             </div>
             <div className="absolute bottom-0 w-screen h-px bg-gray-700"></div>
         </div>
